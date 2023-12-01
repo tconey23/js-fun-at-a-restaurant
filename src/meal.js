b057b790aa2fbd312d4c000360c87b1a4b8b72aa
tom coney <tom.ce.coney@gmail.com>
2023-12-01 16:40:10 -0700

    add order and meal code updates

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,74 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+
+ var menuItemName = `Delicious ${name}`
+ return menuItemName
+
+}
+
+function createMenuItem(name, price, type){
+
+var menuItem = {
+
+name: name,
+price: price,
+type: type
+
+}
+
+return menuItem
+
+
+}
+
+function addIngredients(item, array){
+
+if (array.includes(item)){
+
+
+
+}else{
+  array.push(item)
+  return array
+}
+
+}
+
+function formatPrice(initialPrice){
+
+  var initialPrice = "$" + initialPrice
+
+return initialPrice
+
+}
+
+function decreasePrice(price){
+
+var price = price - (price * .10)
+
+return price 
+
+}
+
+function createRecipe(title,ingredients,type){
+
+  var recipe = {
+
+    title: title,
+    ingredients: ingredients,
+    type: type
+
+  }
+  
+  return recipe
 
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
--- src/order.js
+++ src/order.js
@@ -1,7 +1,67 @@
+function takeOrder(orderNumber,deliveryOrders){
+
+  deliveryOrders.push(orderNumber)
+  
+  if(deliveryOrders.length > 3){
+
+    deliveryOrders.splice(3,1)
+
+    return deliveryOrders
+
+  }else{
+
+    return deliveryOrders
+}
+
+}
+
+function refundOrder(refOrder,deliveryOrders){
+
+  for (var i =0; i < deliveryOrders.length; i++){
+  
+    if(deliveryOrders[i].orderNumber != refOrder){
+    }else{
+      deliveryOrders.splice(i,1)
+    } 
+  }
+}
+
+function listItems(deliveryOrders){
+
+  var outputList = ''
+  for(var i = 0; i < deliveryOrders.length; i++){
+
+    outputList = (`${outputList} ${String(deliveryOrders[i].item)},`)
+
+
+  }
+  return outputList.replace(/,(\s+)?$/, '').trim()
+}
+
+function searchOrder(deliveryOrders,searchTerm){
+  
+  var testResult = null
+
+  for(var i = 0; i < deliveryOrders.length; i++){
+
+    if (deliveryOrders[i].item === searchTerm){
+      return true
+      
+    }else{
+      testResult = false
+
+    }
+
+  }
+
+  return testResult
+
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
